50f3dcfa67cf4d7e8885441c3dc6038266dd3201
Morvin-Ian <morvinian@gmail.com>
2023-06-30 21:34:02 +0300

    controllers and routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        
+        
+        return view('posts',[
+            'posts'=>$posts
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        
+        return view('post',[
+            'posts'=>$post
+        ]);
+    }
+
+    public function category(Category $category)
+    {
+        $posts = Post::where('category_id', $category)->get();
+        
+        return view('category',[
+            'posts'=>$posts
+        ]);
+    }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PostController::class)->group(function(){
+    Route::get('/posts/create-post', 'index');
+    Route::post('/post/{post}/', 'show');
+    Route::delete('/category/{category}', 'category')->name('destroy');
+
 });
