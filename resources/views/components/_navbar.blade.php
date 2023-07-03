1a950e7892c903fcd9d3b762e41b457a7a7c2b7a
Morvin-Ian <morvinian@gmail.com>
2023-07-03 12:25:53 +0300

    final touches

--- resources/views/components/_navbar.blade.php
+++ resources/views/components/_navbar.blade.php
@@ -33,6 +33,8 @@
             </li>
         @endauth
       </ul>
+
+      
       <form method="GET" class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
