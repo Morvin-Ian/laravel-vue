ae269fba3d3dd4ac747f98fc7ebaaac3098a306c
Morvin-Ian <morvinian@gmail.com>
2023-07-11 10:59:35 +0300

    setup for apis

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/unauthorized', function(){
+    return [
+        "message"=>"unauthorized"
+    ];
+
+})->name('unauthorized');
+
+
+// Authentication
+Route::controller(AuthController::class)->group(function(){
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login',  'login')->name('login');
+
+});
+
+
+
+//Routes that require tokens
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/data', [DataController::class, 'data']);
+ 
+
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
